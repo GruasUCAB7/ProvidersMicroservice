fee56ebf56b91a2d53ffc148a06d7426fa52002a
Joyce <joywg14@gmail.com>
2024-10-30 19:52:24 -0400

    getAll-cranes[B7]: adding getAll cranes with filter and pagination

---
+++ ProvidersMicroservice/ProvidersMS/src/Cranes/Application/Queries/GetAll/GetAllCranesQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using ProvidersMS.Core.Application.Services;
+using ProvidersMS.Core.Utils.Result;
+using ProvidersMS.src.Cranes.Application.Queries.GetAll.Types;
+using ProvidersMS.src.Cranes.Application.Queries.Types;
+using ProvidersMS.src.Cranes.Application.Repositories;
+using ProvidersMS.src.Cranes.Application.Exceptions;
+
+namespace ProvidersMS.src.Cranes.Application.Queries.GetAll
+{
+    public class GetAllCranesQueryHandler(ICraneRepository craneRepository) : IService<GetAllCranesQuery, GetCraneResponse[]>
+    {
+        private readonly ICraneRepository _craneRepository = craneRepository;
+        public async Task<Result<GetCraneResponse[]>> Execute(GetAllCranesQuery data)
+        {
+            var crane = await _craneRepository.GetAll(data);
+            if (crane == null)
+            {
+                return Result<GetCraneResponse[]>.Failure(new CraneNotFoundException());
+            }
+
+            var response = crane.Select(crane => new GetCraneResponse(
+                crane.GetId(),
+                crane.GetBrand(),
+                crane.GetModel(),
+                crane.GetPlate(),
+                crane.GetCraneType(),
+                crane.GetYear(),
+                crane.GetIsActive()
+                )
+            ).ToArray();
+
+            return Result<GetCraneResponse[]>.Success(response);
+        }
+    }
+}
---
+++ ProvidersMicroservice/ProvidersMS/src/Cranes/Application/Queries/Types/GetCraneResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace ProvidersMS.src.Cranes.Application.Queries.Types
+{
+    public record GetCraneResponse
+    (
+        string Id,
+        string Brand,
+        string Model,
+        string Plate,
+        string CraneType,
+        int Year,
+        bool IsActive
+    );
+}
